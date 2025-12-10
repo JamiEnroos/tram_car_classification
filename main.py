f6e4ba8058e27d71538cb96c1a54889dccb03249
Jami Enroos <jami.enroos@tuni.fi>
2025-12-10 16:35:58 +0200

    Support vector classifier

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Main
 import os
-
 import librosa as lb
-
 import numpy as np
 import soundfile as sf
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 
 
 def load_audio(path, sr=22050, duration=6.0, mono=True):
@@ -38,15 +41,13 @@ def get_stats(x, features):
     std = np.std(x)
     return np.array([mean, std])
 
-def get_time_domain_features(path, sr, duration=6.0):
-    y, sr = load_audio(path, sr=sr, duration=duration, mono=True)
-
+def get_time_domain_features(y, sr, duration=6.0):
     time_domain_features = np.zeros(0)
     # Time domain
-    zcr = lb.feature.zero_crossings(y)[0]
+    zcr = lb.feature.zero_crossing_rate(y)[0]
     zcr_stats = get_stats(zcr, time_domain_features)
 
-    rms = lb.feature.rms(y=y, sr=sr)[0]
+    rms = lb.feature.rms(y=y)[0]
     rms_stats = get_stats(rms, time_domain_features)
     return np.concatenate((zcr_stats, rms_stats))
 
@@ -70,14 +71,118 @@ def get_frequency_domain_features(y, sr, duration=6.0):
     mfcc_std = mfcc.std(axis=1)
     return np.concatenate((centroid_stats, bw_stats, rolloff_stats, mfcc_mean, mfcc_std))
 
-def build_dataset(folder, sr=22050, duration=6.0):
-    get_time_domain_features()
+def build_dataset(base_dir = "normalized"):
+    X_train_car, y_train_car, X_test_car, y_test_car, X_val_car, y_val_car = read_files(base_dir, "car")
+    X_train_tram, y_train_tram, X_test_tram, y_test_tram, X_val_tram, y_val_tram = read_files(base_dir, "tram")
+
+    X_train = np.vstack([X_train_car, X_train_tram])  # stack rows (files)
+    y_train = np.concatenate([y_train_car, y_train_tram])  # stack labels
+    X_test = np.vstack([X_test_car, X_test_tram])
+    y_test = np.concatenate([y_test_car, y_test_tram])
+    X_val = np.vstack([X_val_car, X_val_tram])
+    y_val = np.concatenate([y_val_car, y_val_tram])
+
+    #print("X shape:", X.shape)  # Should be (n_files, n_features)
+    #print("y shape:", y.shape)  # Should be (n_files)
+
+    # We need to scale the feature data for SVM
+    return X_train, X_test, X_val, y_train, y_test, y_val
+
+def support_vector_classifier(X_train, y_train, X_test, y_test):
+
+    # Scale the features so that they affect the classification equally
+    scaler = StandardScaler()
+    # Params for the SVC
+    params = {
+        'svc__C': [0.01, 0.1, 1, 10, 100],
+        'svc__gamma': ['scale', 0.001, 0.01, 0.1, 1]
+    }
+
+    pipe = make_pipeline(
+        scaler,
+        SVC(kernel='rbf', probability=False)
+    )
+
+    grid = GridSearchCV(
+        estimator=pipe,
+        param_grid=params,
+        cv=5,
+        n_jobs=-1,
+        scoring='accuracy'
+    )
+
+    grid.fit(X_train, y_train)
+
+    print(f"Best Parameters found: {grid.best_params_}")
+    y_pred = grid.predict(X_test)
+
+    print("\nFinal Test Set Results:")
+    print(classification_report(y_test, y_pred))
+
+def read_files(base_dir, vehicle, duration=6.0, sr =22050):
+
+    train_features = []
+    train_labels = []
+    test_features = []
+    test_labels = []
+    val_features = []
+    val_labels = []
+
+    current_label = 100
 
 
-def read_files(folder, vehicle):
-    for dir in os.listdir(f"normalized/{vehicle}/"):
-        if dir == '.DS_Store':
+    for subdir in os.listdir(base_dir):
+
+        subpath = os.path.join(base_dir, subdir)
+        if not os.path.isdir(subpath):
             continue
-        for file in os.listdir(f"normalized/{vehicle}/{dir}"):
-            if file == '.DS_Store':
+        vehicle_dir = os.path.join(subpath, vehicle)
+        if not os.path.isdir(vehicle_dir):
+            continue
+        for filename in os.listdir(vehicle_dir):
+
+            if not filename.lower().endswith(".wav"):
                 continue
+
+            full_path = os.path.join(vehicle_dir, filename)
+
+            y, sr = load_audio(os.path.join(full_path), sr=sr, duration=duration, mono=True)
+            freq_features = get_frequency_domain_features(y, sr, duration=duration) # should be 1D
+            time_features = get_time_domain_features(y, sr, duration=duration)
+
+            combined_vec = np.concatenate([freq_features, time_features], axis=0)
+
+            if vehicle == 'car':
+                current_label = 1
+            elif vehicle == 'tram':
+                current_label = 0
+
+            if current_label == 100:
+                print("invalid label")
+                continue
+
+            if subdir == "train":
+                train_features.append(combined_vec)
+
+                train_labels = np.append(train_labels, current_label)
+            elif subdir == "test":
+                test_features.append(combined_vec)
+                test_labels = np.append(test_labels, current_label)
+            elif subdir == "val":
+                val_features.append(combined_vec)
+                val_labels = np.append(val_labels, current_label)
+
+    X_train = np.vstack(train_features)
+    y_train = np.array(train_labels)
+    X_test = np.vstack(test_features)
+    y_test = np.array(test_labels)
+    X_val = np.vstack(val_features)
+    y_val = np.array(val_labels)
+    return X_train, y_train, X_test, y_test, X_val, y_val
+
+def main():
+    X_train, X_test, X_val, y_train, y_test, y_val = build_dataset()
+    support_vector_classifier(X_train, y_train, X_test, y_test)
+
+if __name__ == "__main__":
+    main()
--- normalize.py
+++ normalize.py
@@ -4,22 +4,35 @@ import librosa as lb
 import soundfile as sf
 
 
-def normalize(vehicle):
+def normalize(vehicle, base_dir="data", wr_dir="normalized"):
     i = 1
-    for dir in os.listdir(f"data/{vehicle}/"):
-        if dir == '.DS_Store':
+    for subdir in os.listdir(base_dir):
+        subpath = os.path.join(base_dir, subdir)
+        if not os.path.isdir(subpath):
             continue
-        for file in os.listdir(f"data/{vehicle}/{dir}"):
-            if file == '.DS_Store':
+
+        vehicle_dir = os.path.join(subpath, vehicle)
+        if not os.path.isdir(vehicle_dir):
+            continue
+
+        for user_dir in os.listdir(vehicle_dir):
+            subpath = os.path.join(vehicle_dir, user_dir)
+            if not os.path.isdir(subpath):
                 continue
-            audio, fs = sf.read(f"data/{vehicle}/{dir}/{file}")
-            print(f"before normalization: {audio}")
-            audio = lb.util.normalize(audio)
-            print(f"after: {audio}")
-            sf.write(f"normalized/{vehicle}/{vehicle}_{i}.wav", audio, fs)
-            # sf.write(f"normalized/{vehicle}/file.wav", audio, fs)
+            for filename in os.listdir(subpath):
+                if not (filename.lower().endswith(".wav") or filename.lower().endswith(".mp3")):
+                    continue
 
-            i += 1
+                full_path = os.path.join(subpath, filename)
 
-normalize("car")
-normalize("tram")
+                audio, fs = sf.read(full_path)
+                print(f"before normalization: {audio}")
+                audio = lb.util.normalize(audio)
+                print(f"after: {audio}")
+                sf.write(f"{wr_dir}/{subdir}/{vehicle}/{vehicle}_{i}.wav", audio, fs)
+
+                i += 1
+
+if __name__ == "__main__":
+    normalize("car")
+    normalize("tram")
